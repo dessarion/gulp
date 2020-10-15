f13bd4fab8917940baa97496a67d52465be33986
dessarion <dessuas@gmial.com>
2020-10-15 20:30:26 +0300

    rebuilded logic, added tasks for js files

---
+++ README.md
@@ -0,0 +1,14 @@
+Use B variable to inject comands (task functions) for "build mode" // gulp build
+Use S variable to inject comands (task functions) for "dev mode" with live server // gulp serve
+
+********
+deleting destination folder // gulp clear
+
+*******
+
+To use a lot of javascript files. Their paths need to be added to the scripts array. (Sequence counts)
+
+*******
+
+
+the #templates folder contains the expanded files
--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,117 @@
-const {src, dest, series, watch} = require('gulp')
-const sass = require('gulp-sass')
-const csso = require('gulp-csso')
-const include = require('gulp-file-include')
-const htmlmin = require('gulp-htmlmin')
-const del = require('del')
-const concat = require('gulp-concat')
-const autoprefixer = require('gulp-autoprefixer')
-const sync = require('browser-sync').create()
+const { src, dest, series, watch } = require("gulp");
+const sass = require("gulp-sass");
+const csso = require("gulp-csso");
+const include = require("gulp-file-include");
+const htmlmin = require("gulp-htmlmin");
+const del = require("del");
+const concat = require("gulp-concat");
+const autoprefixer = require("gulp-autoprefixer");
+const rename = require("gulp-rename");
+const groupMedia = require("gulp-group-css-media-queries");
+const uglify = require("gulp-uglify-es").default;
+const sync = require("browser-sync").create();
 
-const source = 'src'
-const destination = 'dist'
+const source = "src";
+const destinationFolderName = {
+  default: "dist",
+};
+const destination = destinationFolderName.default;
+
+const scripts = [
+  `${source}/scripts/index.js`,
+  `${source}/scripts/second.js`,
+  `${source}/scripts/third.js`,
+];
 
 const path = {
   s: {
     html: `${source}/**.html`,
-    scss: `${source}/scss/**.scss`
+    scss: `${source}/scss/index.scss`,
   },
   d: {
-    scss: `${destination}/css`
+    scss: `${destination}/css`,
+    js: `${destination}/scripts`,
+    htmlTemplates: `${destination}/#templates`,
+    cssTemplates: `${destination}/#templates/css`,
+    jsTemplates: `${destination}/#templates/scripts`,
   },
   w: {
     html: `${source}/**/*.html`,
-    scss: `${source}/scss/**.scss`
-  }
-}
+    scss: `${source}/scss/**/*.scss`,
+    js: `${source}/scripts/**/*.js`,
+  },
+};
 
 function html() {
   return src(path.s.html)
-    .pipe(include({
-      prefix: '@@'
-    }))
-    .pipe(htmlmin({
-      collapseWhitespace: true
-    }))
-    .pipe(dest(destination))
+    .pipe(
+      include({
+        prefix: "@@",
+      })
+    )
+    .pipe(dest(path.d.htmlTemplates))
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+      })
+    )
+    .pipe(dest(destination));
 }
 
 function scss() {
   return src(path.s.scss)
-    .pipe(sass())
-    .pipe(autoprefixer({
-      cascade: true
-    }))
+    .pipe(
+      sass({
+        outputStyle: "expanded",
+      })
+    )
+    .pipe(groupMedia())
+    .pipe(
+      autoprefixer({
+        cascade: true,
+      })
+    )
+    .pipe(dest(path.d.cssTemplates))
     .pipe(csso())
-    .pipe(concat('index.css'))
-    .pipe(dest(path.d.scss))
+    .pipe(
+      rename({
+        extname: ".min.css",
+      })
+    )
+    .pipe(dest(path.d.scss));
+}
+ 
+function js() {
+  return src(scripts)
+    .pipe(concat("index.js"))
+    .pipe(dest(path.d.jsTemplates))
+    .pipe(uglify())
+    .pipe(
+      rename({
+        extname: ".min.js",
+      })
+    )
+    .pipe(dest(path.d.js));
 }
 
 function serve() {
   sync.init({
     server: `./${destination}`
-  })
+  });
 
-  watch(path.w.html, series(html)).on('change', sync.reload)
-  watch(path.w.html, series(scss)).on('change', sync.reload)
+  watch(path.w.html, series(html)).on("change", sync.reload);
+  watch(path.w.scss, series(scss)).on("change", sync.reload);
+  watch(path.w.js, series(js)).on("change", sync.reload);
 }
 
 function clear() {
-  return del(destination)
+  return del(destination);
 }
 
-exports.build = series(clear, scss, html)
-exports.serve = series(clear, scss, html, serve)
-exports.clear = clear
+const B = [clear, scss, html, js];
+const S = [clear, scss, html, js, serve];
+
+exports.build = series(...B);
+exports.serve = series(...S);
+
+exports.clear = clear;
