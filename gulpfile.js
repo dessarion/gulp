9ff9323d10d786bd6c62bc39ccb1923fed62ac44
dessarion <dessuas@gmial.com>
2020-10-17 17:31:51 +0300

    finished gulp constructor

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,19 @@ const autoprefixer = require("gulp-autoprefixer");
 const rename = require("gulp-rename");
 const groupMedia = require("gulp-group-css-media-queries");
 const uglify = require("gulp-uglify-es").default;
+const imagemin = require("gulp-imagemin");
+const svgSprite = require("gulp-svg-sprite");
 const sync = require("browser-sync").create();
+const ttf2woff = require("gulp-ttf2woff");
+const ttf2woff2 = require("gulp-ttf2woff2");
+const fonter = require("gulp-fonter");
 
 const source = "src";
 const destinationFolderName = {
   default: "dist",
+  folderName: require('path').basename(__dirname)
 };
-const destination = destinationFolderName.default;
+const destination = destinationFolderName.folderName;
 
 const scripts = [
   `${source}/scripts/index.js`,
@@ -25,12 +31,21 @@ const scripts = [
 
 const path = {
   s: {
-    html: `${source}/**.html`,
+    html: `${source}/*.html`,
     scss: `${source}/scss/index.scss`,
+    img: [
+      `${source}/img/**/*.{jpg,png,svg,gif,ico,webp}`,
+      `!${source}/img/icons/*.svg`,
+    ],
+    sprites: `${source}/img/icons/*.svg`,
+    fonts: `${source}/fonts/*.ttf`,
   },
   d: {
     scss: `${destination}/css`,
     js: `${destination}/scripts`,
+    img: `${destination}/img`,
+    sprites: `${source}/img/sprites`,
+    fonts: `${destination}/fonts`,
     htmlTemplates: `${destination}/#templates`,
     cssTemplates: `${destination}/#templates/css`,
     jsTemplates: `${destination}/#templates/scripts`,
@@ -41,6 +56,8 @@ const path = {
     js: `${source}/scripts/**/*.js`,
   },
 };
+
+const spriteName = "sprite";
 
 function html() {
   return src(path.s.html)
@@ -80,7 +97,7 @@ function scss() {
     )
     .pipe(dest(path.d.scss));
 }
- 
+
 function js() {
   return src(scripts)
     .pipe(concat("index.js"))
@@ -94,24 +111,75 @@ function js() {
     .pipe(dest(path.d.js));
 }
 
+function images() {
+  return src(path.s.img)
+    .pipe(
+      imagemin({
+        progressive: true,
+        svgoPlugins: [{ removeVievBox: false }],
+        interlaced: true,
+        optimizationLevel: 3, // 0 - 7
+      })
+    )
+    .pipe(dest(path.d.img));
+}
+
+function fonts() {
+  return src(path.s.fonts)
+    .pipe(ttf2woff())
+    .pipe(dest(path.d.fonts))
+    .pipe(src(path.s.fonts))
+    .pipe(ttf2woff2())
+    .pipe(dest(path.d.fonts));
+}
+
 function serve() {
   sync.init({
-    server: `./${destination}`
+    server: `./${destination}`,
   });
 
   watch(path.w.html, series(html)).on("change", sync.reload);
   watch(path.w.scss, series(scss)).on("change", sync.reload);
   watch(path.w.js, series(js)).on("change", sync.reload);
+  watch(path.s.img, series(images)).on("change", sync.reload);
+}
+
+function sprites() {
+  return src(path.s.sprites)
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: `../icons/${spriteName}.svg`,
+            example: true, // creates previev file
+          },
+        },
+      })
+    )
+    .pipe(dest(path.d.sprites));
+}
+
+function otf2ttf() {
+  return src(`${source}/fonts/*.otf`)
+    .pipe(
+      fonter({
+        formats: ["ttf"],
+      })
+    )
+    .pipe(dest(`${source}/fonts`));
 }
 
 function clear() {
   return del(destination);
 }
 
-const B = [clear, scss, html, js];
-const S = [clear, scss, html, js, serve];
+const B = [clear, scss, html, js, images, fonts];
+const S = [clear, scss, html, js, images, fonts, serve];
 
 exports.build = series(...B);
 exports.serve = series(...S);
 
 exports.clear = clear;
+
+exports.sprites = sprites;
+exports.otf2ttf = otf2ttf;
